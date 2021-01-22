cd303d250cb83c3a3e9087dfe3a90afc78f32c6d
unknown <ptjh0309@hanmail.net>
2021-01-22 17:32:56 +0900

    Modify PTFFileReadService Constructor

--- MELCORUncertaintyHelper/Service/PTFFileReadService.cs
+++ MELCORUncertaintyHelper/Service/PTFFileReadService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MELCORUncertaintyHelper.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,11 @@ namespace MELCORUncertaintyHelper.Service
 {
     public class PTFFileReadService
     {
-        public PTFFileReadService()
-        {
+        private PTFFile[] files;
 
+        public PTFFileReadService(object files)
+        {
+            this.files = (PTFFile[])files;
         }
     }
 }

4197f4605e0263e76095c4c09cddf7e2a55655d8
counterAttacking <ptjh0309@hanmail.net>
2021-01-22 15:28:04 +0900

    Add LogFileWriteService

---
+++ MELCORUncertaintyHelper/Service/LogFileWriteService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MELCORUncertaintyHelper.Service
+{
+    public class LogFileWriteService
+    {
+        private Exception exception;
+
+        public LogFileWriteService(Exception ex)
+        {
+            this.exception = ex;
+        }
+
+        public void MakeLogFile()
+        {
+            var msg = new StringBuilder();
+            msg.Append(this.exception.ToString());
+
+            var fileName = new StringBuilder();
+            fileName.Append(DateTime.Now.ToString("yyyyMMddHHmms"));
+            fileName.Append("_Log.txt");
+
+            var filePath = Path.Combine(Environment.CurrentDirectory, "log", fileName.ToString());
+            File.WriteAllText(filePath, msg.ToString());
+        }
+    }
+}
--- MELCORUncertaintyHelper/Service/PTFFileOpenService.cs
+++ MELCORUncertaintyHelper/Service/PTFFileOpenService.cs
@@ -43,7 +43,8 @@ namespace MELCORUncertaintyHelper.Service
             }
             catch (Exception ex)
             {
-
+                var logWrite = new LogFileWriteService(ex);
+                logWrite.MakeLogFile();
             }
             finally
             {

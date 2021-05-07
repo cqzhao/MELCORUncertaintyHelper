165054ad286bc142d5dca38468141f4f9e57911b
counterAttacking <ptjh0309@hanmail.net>
2021-05-07 16:43:07 +0900

    Delete unnecessary codes

--- MELCORUncertaintyHelper/Service/InputVariableReadService.cs
+++ MELCORUncertaintyHelper/Service/InputVariableReadService.cs
@@ -10,11 +10,6 @@ namespace MELCORUncertaintyHelper.Service
 {
     public class InputVariableReadService
     {
-        private string[] inputPackageNames;
-        private int[] inputControlVolumes;
-        private int[] idxes;
-        private int[] totalIdxes;
-
         private string[] inputVariables;
         private string[] inputPlotKeys;
         private int[] inputIndexes;
@@ -35,15 +30,7 @@ namespace MELCORUncertaintyHelper.Service
             }
         }
 
-        public Object GetIdxes() => this.idxes.Clone();
-
-        public Object GetTotalIdxes() => this.totalIdxes.Clone();
-
         public Object GetInputVariables() => this.inputVariables.Clone();
-
-        public Object GetInputPlotKeys() => this.inputPlotKeys.Clone();
-
-        public Object GetInputIndexes() => this.inputIndexes.Clone();
 
         public Object GetInputTRIndexes() => this.inputTRIndexes.Clone();
 
@@ -109,32 +96,6 @@ namespace MELCORUncertaintyHelper.Service
         {
             try
             {
-                var packageNames = new List<string>();
-                var controlVolumes = new List<int>();
-
-                for (var i = 0; i < this.inputVariables.Length; i++)
-                {
-                    string name;
-                    int node;
-
-                    if (this.inputVariables[i].Contains("."))
-                    {
-                        name = this.inputVariables[i].Substring(0, this.inputVariables[i].LastIndexOf("."));
-                        node = Convert.ToInt32(this.inputVariables[i].Substring(this.inputVariables[i].LastIndexOf(".") + 1));
-                    }
-                    else
-                    {
-                        name = this.inputVariables[i];
-                        node = 0;
-                    }
-
-                    packageNames.Add(name);
-                    controlVolumes.Add(node);
-                }
-
-                this.inputPackageNames = packageNames.ToArray();
-                this.inputControlVolumes = controlVolumes.ToArray();
-
                 var inputPlotKeys = new List<string>();
                 var inputIndexes = new List<int>();
 
@@ -162,57 +123,6 @@ namespace MELCORUncertaintyHelper.Service
 
                 this.inputPlotKeys = inputPlotKeys.ToArray();
                 this.inputIndexes = inputIndexes.ToArray();
-            }
-            catch (Exception ex)
-            {
-                var logWrite = new LogFileWriteService(ex);
-                logWrite.MakeLogFile();
-            }
-        }
-
-        public void MakeIndexes(string[] packageNames, int[] packageVariableCnt, int[] controlVolumes)
-        {
-            try
-            {
-                var idxes = new List<int>();
-                var totalIdxes = new List<int>();
-
-                int frontIdx;
-                int rearIdx;
-                int totalIdx;
-
-                for (var i = 0; i < this.inputVariables.Length; i++)
-                {
-                    frontIdx = Array.IndexOf(packageNames, this.inputPackageNames[i]);
-                    rearIdx = Array.LastIndexOf(packageNames, this.inputPackageNames[i]);
-                    if (frontIdx == -1)
-                    {
-                        frontIdx = Array.IndexOf(packageNames, this.inputVariables[i]);
-                        if (frontIdx == -1)
-                        {
-                            totalIdx = packageVariableCnt[frontIdx] - 1;
-                        }
-                        else
-                        {
-                            totalIdx = -1;
-                        }
-                    }
-                    else if (frontIdx == rearIdx)
-                    {
-                        totalIdx = Array.IndexOf(controlVolumes, this.inputControlVolumes[i],
-                            packageVariableCnt[frontIdx] - 1, packageVariableCnt[frontIdx + 1] - packageVariableCnt[frontIdx]);
-                    }
-                    else
-                    {
-                        totalIdx = Array.IndexOf(controlVolumes, this.inputControlVolumes[i], frontIdx, rearIdx);
-                    }
-
-                    idxes.Add(frontIdx);
-                    totalIdxes.Add(totalIdx);
-                }
-
-                this.idxes = idxes.ToArray();
-                this.totalIdxes = totalIdxes.ToArray();
             }
             catch (Exception ex)
             {
